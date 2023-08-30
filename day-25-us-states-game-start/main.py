f8420211513ff77c7e6c141c03aa49e88c2458d1
LJW92 <ljw9282@foxmail.com>
2023-08-30 17:34:32 +1000

    guess the state project completed

---
+++ day-25-us-states-game-start/main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+data = pandas.read_csv('50_states.csv')
+state_list = data.state.to_list()
+print(state_list)
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = 'blank_states_img.gif'
+screen.addshape(image)
+turtle.shape(image)
+
+mark = turtle.Turtle()
+mark.color('black')
+mark.speed('fastest')
+mark.penup()
+mark.hideturtle()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer = screen.textinput(title="Guess the State", prompt="What's another state's name")
+    answer = answer.title()
+
+    if answer == 'Exit':
+        missing_state = []
+        for state in state_list:
+            if state not in guessed_states:
+                missing_state.append(state)
+        new_data = pandas.DataFrame(missing_state)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer in state_list:
+        guessed_states.append(answer)
+        state_data = data[data.state == answer]
+        mark.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        mark.write(answer)
+
+screen.exitonclick()
