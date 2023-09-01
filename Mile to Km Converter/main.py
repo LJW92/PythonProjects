d0c7a702a4f8b1a1eb700f6980a2d140c4a76bcf
LJW92 <ljw9282@foxmail.com>
2023-09-01 15:45:32 +1000

    timer project study

--- Mile to Km Converter/main.py
+++ Mile to Km Converter/main.py
@@ -11,6 +11,7 @@ window = Tk()
 window.title("Mile to Km converter")
 window.config(padx=25, pady=25)
 
+
 label_1 = Label(text=' Miles', font=('Arial', 18))
 label_2 = Label(text='is equal to', font=('Arial', 18))
 label_3 = Label(text='0', font=('Arial', 18))
@@ -26,5 +27,7 @@ button_1.grid(column=1, row=2)
 
 user_input_1 = Entry(width=15)
 user_input_1.grid(column=1, row=0)
+
+
 
 window.mainloop()
